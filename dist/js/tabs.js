bd3c44bfa4ac17d8df230fe0ff9a419ed661cb03
Lornz18 <mellgodwinbarza22@gmail.com>
2024-08-21 16:01:39 +0800

    dinosaur

---
+++ dist/js/tabs.js
@@ -0,0 +1,24 @@
+const tabs = document.querySelectorAll(".fave__tabs h4");
+const contents = document.querySelectorAll(".fave__details");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeActiveTab();
+    tab.classList.add("active");
+    const activeContent = document.querySelector(`#${tab.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveTab() {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,21 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {},
+      fontFamily: {
+        poppins: "Poppins",
+      },
       colors: {
         primary: "#00754A",
         secondary: "#D4E9E2",
         light: "#FFFFFF",
         dark: "#1E3932",
+        black: "#000000",
+        mint: "#F1F8F5",
         // tertiary: "#223c5f",
+      },
+      backgroundImage: {
+        bgReward:
+          "url('../img/455044342_1184467975940622_4744910648108961620_n.webp')",
       },
     },
   },
